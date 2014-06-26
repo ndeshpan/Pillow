d034c74f8852c7201785d8f4084768e6550fdf8e
hugovk <hugovk@users.noreply.github.com>
2014-06-26 23:20:47 +0300

    Add ImageTk.BitmapImage test

--- Tests/test_imagetk.py
+++ Tests/test_imagetk.py
@@ -1,14 +1,38 @@
-from helper import unittest, PillowTestCase, tearDownModule
+from helper import unittest, PillowTestCase, tearDownModule, lena
+
+try:
+    from Tkinter import Tk
+    from PIL import ImageTk
+    dir(ImageTk)
+except (OSError, ImportError) as v:
+    # Will be skipped in setUp
+    pass
 
 
 class TestImageTk(PillowTestCase):
 
-    def test_import(self):
+    def setUp(self):
         try:
+            from Tkinter import Tk
             from PIL import ImageTk
-            dir(ImageTk)
         except (OSError, ImportError) as v:
             self.skipTest(v)
+
+    def test_dir(self):
+        dir(ImageTk)
+
+    def test_bitmapimage_from_pil_image(self):
+        # Arrange
+        im = lena("1")
+        window = Tk()
+        w, h = im.size
+
+        # Act
+        bitmap_image = ImageTk.BitmapImage(image=im)
+
+        # Assert
+        self.assertEqual(h, bitmap_image.height())
+        self.assertEqual(w, bitmap_image.width())
 
 
 if __name__ == '__main__':
