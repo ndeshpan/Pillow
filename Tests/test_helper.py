bebbd77e7703a2648517c8f7129ae19e29d07563
hugovk <hugovk@users.noreply.github.com>
2014-08-15 17:35:04 +0300

    Add a failing test for faulty caching. Passes with caching off.

--- Tests/test_helper.py
+++ Tests/test_helper.py
@@ -1,5 +1,7 @@
 from helper import unittest, PillowTestCase, lena
 
+from PIL import Image
+
 
 class TestImage(PillowTestCase):
 
@@ -7,17 +9,20 @@ class TestImage(PillowTestCase):
         # Make sure lena() returns the same thing
 
         # Arrange
+        im_original = Image.open("Tests/images/lena.ppm")
         im1 = lena()
         im2 = lena()
 
         # Assert
         self.assert_image_equal(im1, im2)
+        self.assert_image_equal(im1, im_original)
 
     def test_lena_changed(self):
         # Make sure lena() always returns the same thing,
         # no matter if you change the returned one
 
         # Arrange
+        im_original = Image.open("Tests/images/lena.ppm")
         im0 = lena()
         im1 = lena()
 
@@ -29,6 +34,25 @@ class TestImage(PillowTestCase):
 
         # Assert
         self.assert_image_equal(im0, im2)
+        self.assert_image_equal(im0, im_original)
+
+    def test_lena_change_pixel(self):
+        # Make sure lena() always returns the same thing,
+        # no matter if you change the returned one
+
+        # Arrange
+        im_original = Image.open("Tests/images/lena.ppm")
+        im0 = lena()
+
+        # Act
+        # Change a pixel in im0
+        im0.load()
+        im0.putpixel((20, 20), im0.getpixel((0, 0)))
+        # Get a fresh lena()
+        im1 = lena()
+
+        # Assert
+        self.assert_image_equal(im1, im_original)
 
 if __name__ == '__main__':
     unittest.main()
