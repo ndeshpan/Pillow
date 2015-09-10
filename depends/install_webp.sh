59e1669d1906c0c44795a9d4c72a44fbd15a3d75
hugovk <hugovk@users.noreply.github.com>
2015-09-10 22:19:10 +0300

    Check binaries

--- depends/install_webp.sh
+++ depends/install_webp.sh
@@ -2,14 +2,19 @@
 # install webp
 
 if [ ! -f libwebp-0.4.3.tar.gz ]; then
-    wget 'http://downloads.webmproject.org/releases/webp/libwebp-0.4.3.tar.gz'
+#     wget 'http://downloads.webmproject.org/releases/webp/libwebp-0.4.3.tar.gz'
+    'wget http://downloads.webmproject.org/releases/webp/libwebp-0.4.3-linux-x86-32.tar.gz'
 fi
 
-rm -r libwebp-0.4.3
-tar -xvzf libwebp-0.4.3.tar.gz
 
-pushd libwebp-0.4.3
 
-./configure --prefix=/usr --enable-libwebpmux --enable-libwebpdemux && make -j4 && sudo make -j4 install
+# rm -r libwebp-0.4.3
+# tar -xvzf libwebp-0.4.3.tar.gz
+rm -r libwebp-0.4.3-linux-x86-32
+tar -xvzf libwebp-0.4.3-linux-x86-32.tar.gz
+
+pushd libwebp-0.4.3-linux-x86-32
+
+ls
 
 popd
