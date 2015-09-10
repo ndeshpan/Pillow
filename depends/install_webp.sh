5056bee3757a606340fbcc7a4dccf84d29a7d9d1
hugovk <hugovk@users.noreply.github.com>
2015-09-10 22:45:43 +0300

    tar into ~

--- depends/install_webp.sh
+++ depends/install_webp.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 # install webp
 
+pushd ~
+
 if [ ! -f libwebp-0.4.3.tar.gz ]; then
 #     wget 'http://downloads.webmproject.org/releases/webp/libwebp-0.4.3.tar.gz'
     wget 'http://downloads.webmproject.org/releases/webp/libwebp-0.4.3-linux-x86-32.tar.gz'
 fi
 
-
+popd
 
 # rm -r libwebp-0.4.3
 # tar -xvzf libwebp-0.4.3.tar.gz
 rm -r libwebp-0.4.3-linux-x86-32
-tar -xvzf libwebp-0.4.3-linux-x86-32.tar.gz
+tar -xvzf libwebp-0.4.3-linux-x86-32.tar.gz  -C ~
 
-pushd libwebp-0.4.3-linux-x86-32
-
-ls
-
-popd
+# pushd libwebp-0.4.3-linux-x86-32
+#
+# ls
+#
+# popd
